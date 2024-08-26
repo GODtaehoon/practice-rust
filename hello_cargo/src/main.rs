81bafc695c876c14accc63cf742541100dd05304
GODtaehoon <jth1315@naver.com>
2024-08-26 13:56:34 +0900

    study first

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, 😱!");
+    let my_num: u8 = 100;
+    let my_other_num: u8 = 50;
+    let my_third: u8 = my_num + my_other_num;
+
+    println!("umm {}", my_third)
 }
