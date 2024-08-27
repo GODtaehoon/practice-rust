3b2b5827124e3d76189adadc5f85db5e0b7e5c2e
GODtaehoon <jth1315@naver.com>
2024-08-27 22:52:18 +0900

    study

--- mars_calc/src/main.rs
+++ mars_calc/src/main.rs
@@ -1,5 +1,11 @@
+use std::io;
+
 fn main() {
-    println!("Weight on Mars: {}kg",calc_weight_on_mars(100.0));
+    let mut input = String::new();
+    io::stdin().read_line(&mut input);
+    println!("Input: {}", input);
+    let mars_weight = calc_weight_on_mars(100.0);
+    println!("Weight on Mars: {}kg",mars_weight);
 }
 
 fn calc_weight_on_mars(weight: f32) -> f32 {
