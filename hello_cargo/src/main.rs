8f411364013eb45dfac2e7c1b15b898fc337a110
GODtaehoon <jth1315@naver.com>
2024-08-29 19:00:35 +0900

    study x

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,7 +1,10 @@
 fn main() {
-    let my_num: u8 = 100;
-    let my_other_num: u8 = 50;
-    let my_third: u8 = my_num + my_other_num;
-
-    println!("umm {}", my_third)
+    let x = 4;
+    println!("x is : {}", x);
+    {
+        let x = 1;
+        println!("x is : {}", x);
+    }
+    let x = x + 2;
+    println!("x is : {}", x);
 }
