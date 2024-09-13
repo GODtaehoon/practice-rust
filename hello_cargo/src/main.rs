898d2d94f8e8f37a5ab6cfc03bc6434e378b2fb2
GODtaehoon <jth1315@naver.com>
2024-09-13 23:32:20 +0900

    study

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,7 +1,4 @@
 fn main() {
-    let my_num: u8 = 100;
-    let my_other_num: u8 = 50;
-    let my_third: u8 = my_num + my_other_num;
-
-    println!("umm {}", my_third)
+    let cond = 2 < 3;
+    println!("umm {}", cond)
 }
